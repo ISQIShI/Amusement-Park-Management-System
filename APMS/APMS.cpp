87ddfb9324ba970b688f7b7d65a7d809fa5e2bb2
ISQIShI <3082847097@qq.com>
2024-05-02 18:14:49 +0800

    1

--- APMS/APMS.cpp
+++ APMS/APMS.cpp
@@ -2,10 +2,20 @@
 #include<tchar.h>
 #include<iostream>
 
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPreInstance,LPTSTR lpCmdLine, int nShowCmd) 
-{
+
+//打开我的github项目页面
+void OpenMyGitHubProjectPage(){
+	SHELLEXECUTEINFO apms = { 0 };
+	apms.cbSize = sizeof(SHELLEXECUTEINFO);
+	apms.lpVerb = _T("open");
+	apms.lpFile = _T("https://github.com/ISQIShI/Amusement-Park-Management-System");
+	apms.nShow = SW_SHOW;
+	ShellExecuteEx(&apms);
+}
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPreInstance,LPTSTR lpCmdLine, INT nShowCmd) 
+{	
 	
 	MessageBox(NULL, _T("你好"), _T("世界"), MB_YESNO);
-	system("pause");
 	return 0;
 }
